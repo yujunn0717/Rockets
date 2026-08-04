a0abbb65a313bc58fb4d488ea04eaea35edf2b99
yujunn0717 <yoojh0717@gmail.com>
2026-08-04 19:21:39 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,31 +1,33 @@
-const CACHE = 'lineup-maker-pwa-v1.0.1';
+const CACHE = 'lineup-maker-pwa-v1.0.4';
 const ASSETS = [
   './',
   'index.html',
-  'styles.css',
-  'app.js',
-  'manifest.webmanifest',
+  'styles.css?v=104',
+  'app.js?v=104',
+  'manifest.webmanifest?v=104',
   'baseball-field.png',
-  'icon-192.png',
+  'icon-192.png?v=104',
   'icon-512.png'
 ];
-
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE).map(key => caches.delete(key))
-    ))
-  );
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))),
+    self.clients.claim()
+  ]));
 });
-
 self.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(fetch(event.request).catch(() => caches.match('./')));
+  const req = event.request;
+  if (req.mode === 'navigate') {
+    event.respondWith(fetch(req, {cache:'no-store'}).then(res => {
+      const copy=res.clone(); caches.open(CACHE).then(c=>c.put('./',copy)); return res;
+    }).catch(()=>caches.match('./')));
     return;
   }
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(fetch(req).then(res=>{
+    const copy=res.clone(); caches.open(CACHE).then(c=>c.put(req,copy)); return res;
+  }).catch(()=>caches.match(req)));
 });
