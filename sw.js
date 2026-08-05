bf318021b4003b9660e5d612c78200aa4d00fbef
yujunn0717 <yoojh0717@gmail.com>
2026-08-05 12:38:59 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,30 @@
-const CACHE = 'rockets-lineup-manager-v10210';
+const CACHE = 'rockets-lineup-manager-v10213';
 const ASSETS = [
   './',
   'index.html',
-  'styles.css',
-  'app.js',
+  'styles-v10212.css',
+  'app-v10212.js',
   'manifest.webmanifest',
-  'baseball-field.png','rockets-watermark.png',
+  'baseball-field.png',
+  'rockets-watermark.png',
   'icon-192.png',
-  'icon-512.png'
+  'icon-512.png',
+  'apple-touch-icon.png',
+  'favicon-32.png'
 ];
-
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE).map(key => caches.delete(key))
-    ))
-  );
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))),
+    self.clients.claim()
+  ]));
 });
-
 self.addEventListener('fetch', event => {
   if (event.request.mode === 'navigate') {
-    event.respondWith(fetch(event.request).catch(() => caches.match('./')));
+    event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./',copy));return response}).catch(() => caches.match('./')));
     return;
   }
   event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
